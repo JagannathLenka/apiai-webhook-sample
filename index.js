3f3f622c68817c1208c801ab87e5734ad83e3066
JagannathLenka <jagannath_lenka@yahoo.com>
2017-04-15 15:42:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ restService.post('/hook', function (req, res) {
                 }
 
                 if (requestBody.result.action) {
-                    speech += requestbody.result;
+                    speech += requestBody.result;
                     speech += 'action: ' + requestBody.result.action;
                 }
             }
