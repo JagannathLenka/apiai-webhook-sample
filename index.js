157332ebbe52ef2bdf2d8d1b2d3d81dcc18c4605
JagannathLenka <jagannath_lenka@yahoo.com>
2017-04-16 20:34:22 -0700

    Update index.js

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ function salesByBrand(parameters) {
       speech += 'sales for ' + parameters.brand + ' $800K already, trends similar to last year. Hopefully it will be better after 10 am'
       break;        
     default:
-      speech += 'sorry, I am not able to find any sales for ' + parameters.brand ;
+      speech += 'sorry, I am not able to find any sales for ' + parameters.brand + ' Do you want to check any other brand' ;
       setRepeat(true);
 }
     
