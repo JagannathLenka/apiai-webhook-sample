9ffabd8036fb4a4c51f8c1b97215b9ca8b26d0e7
mitaliswain <JagannathLenka@Jagannaths-MacBook-Pro.local>
2017-04-30 08:59:12 -0700

    experimenting with follow up

--- index.js
+++ index.js
@@ -110,12 +110,11 @@ function getResponse(speech, parameters) {
 function overallSales(parameters) {
     var speech = '' ;
 
-    console.log(parameters.brand)
-    
     switch(true) {
       case (parameters.brand) :
         speech = 'sales for ' + parameters.date + ' is good compared to last year. Do you want to know the sales for any specific brand?';
         setFollowupEvent('overall-sales-brand');
+        console.log(getFollowupEvent());
         break;
       default :
         speech = 'sales for ' + parameters.date + ' is good compared to last year. Do you want to know the sales for any specific brand?';
